96e8664a246d6698eedf8d827fa6a8090c34e399
jencinas <javierencinasrius@gmail.com>
2013-07-12 05:35:56 +0000

    Now with index added

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var indexbuf = new Buffer(100);
+  indexbuf.write(fs.readFileSync('index.html').toString());
+  
+  response.send(indexbuf.toString());
 });
 
 var port = process.env.PORT || 5000;
